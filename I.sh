64e032afb5e0cd8b2a8ce90d5917089021a0eadd
Regyl1 <roma.orsha@mail.ru>
2020-12-23 16:53:10 +0300

    start

--- I.sh
+++ I.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-
+date_time=$(date +"%d.%m.%y_%T")
+mkdir ~/test/ 2>/dev/null && echo "catalog test was created successfully" > ~/report && touch > ~/test/${date_time}
+ping "www.net_nikogo.ru" 2>/dev/null || echo "${date_time} ERROR:Site is down" >> ~/report
---
+++ IVinf.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+x=2
+while true;
+do
+x=$(($x*2))
+done
